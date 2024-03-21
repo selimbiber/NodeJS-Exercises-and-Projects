c8edd0fe03c5a6620e94c9a1eb2221efdbd76f29
Selim Biber <selim_biber@hotmail.com>
2024-03-21 23:01:10 +0300

    Finish the second Node.js exercise

---
+++ BlogPostCreatingAndSorting/index.js
@@ -0,0 +1,37 @@
+/* 
+    Konsol ekranında postlarımızı sıralayalım, sonrasında yeni bir post oluşturalım ve yeni post ile birlikte postlarımızı tekrar sıralayalım.
+*/
+
+const posts = [
+  { name: "Blog 1", id: 1 },
+  { name: "Blog 2", id: 2 },
+  { name: "Blog 3", id: 3 },
+];
+
+const listPosts = () => {
+  console.log("Current posts:");
+  posts.forEach((post) => console.log(post));
+};
+
+const addPost = (newPost) => {
+  return new Promise((resolve, reject) => {
+    if (newPost && newPost.name && newPost.id) {
+      posts.push(newPost);
+      return resolve("Post added successfully.");
+    }
+    reject(new Error("Invalid post data."));
+  });
+};
+
+async function blogControl(post) {
+  try {
+    const result = await addPost(post);
+    console.log(result);
+    listPosts();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+blogControl({ name: "Blog 4", id: 4 });
+blogControl({ name: "Blog 5" }); // Error: Post couldn't added
